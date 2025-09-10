246253aea20d114000e34d76776221548689b0e6
VeronikaPoluektova08 <veronikapoluektova398@gmail.com>
2025-09-10 16:05:56 +0500

    Add files via upload

--- create_library_db.py
+++ create_library_db.py
@@ -1,61 +1,38 @@
 import sqlite3
-from datetime import date
 
-# Подключаемся к базе данных (если её нет — создаётся)
-conn = sqlite3.connect("online_library.db")
-cursor = conn.cursor()
+def create_db():
+    conn = sqlite3.connect('library.db')
+    cursor = conn.cursor()
 
-# Создание таблицы Users
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    registration_date DATE NOT NULL
-);
-""")
+    # Создаем таблицы
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Students (
+        StudentID INTEGER PRIMARY KEY AUTOINCREMENT,
+        FirstName TEXT NOT NULL,
+        LastName TEXT NOT NULL
+    )
+    ''')
 
-# Таблица Authors
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Authors (
-    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    full_name TEXT NOT NULL,
-    birth_year INTEGER
-);
-""")
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Courses (
+        CourseID INTEGER PRIMARY KEY AUTOINCREMENT,
+        CourseName TEXT NOT NULL
+    )
+    ''')
 
-# Таблица Genres
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Genres (
-    genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-);
-""")
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Enrollments (
+        EnrollmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+        StudentID INTEGER,
+        CourseID INTEGER,
+        FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+        FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+    )
+    ''')
 
-# Таблица Books
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Books (
-    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    publish_year INTEGER,
-    author_id INTEGER,
-    genre_id INTEGER,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
-    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
-);
-""")
+    conn.commit()
+    conn.close()
 
-# Таблица Reviews
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Reviews (
-    review_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    book_id INTEGER,
-    rating INTEGER CHECK (rating >= 1 AND rating <= 5),                                                                                                 
-    comment TEXT,
-    review_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
-""")
+if __name__ == '__main__':
+    create_db()
+    print("База данных и таблицы созданы.")
